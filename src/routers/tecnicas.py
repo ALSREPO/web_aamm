3a8950ae4642f3c6a222a7e9fe8ef6fd6ffc3d1a
ALSREPO <angel.lopez.soto@gmail.com>
2026-05-29 18:44:41 +0200

    feat(backend-tecnicas): creación endpoints para eliminar etiquetas y disciplinas

--- src/routers/tecnicas.py
+++ src/routers/tecnicas.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.orm import Session
 from src.models.models import Usuario, Disciplina, Etiqueta
 from src.schemas.tecnicas import DisciplinaBase, EtiquetaBase
@@ -27,6 +27,7 @@ def obtener_disciplinas(db: Session = Depends(get_read_db)):
 def obtener_etiquetas(db: Session = Depends(get_read_db)):
     return db.query(Etiqueta).all()
 
+
 # Crear disciplinas y etiquetas
 @router.post("/disciplinas", dependencies=[Depends(verificar_admin)])
 def crear_disciplina(nombre: str, db: Session = Depends(get_write_db), admin: Usuario = Depends(usuario_obligatorio)):
@@ -43,3 +44,31 @@ def crear_etiqueta(nombre: str, db: Session = Depends(get_write_db), admin: Usua
     db.commit()
     logger.info(f"Nueva etiqueta creada {nueva.idetiqueta} - {nueva.etiqueta}, por admin {admin.idusuario} - {admin.email}")
     return nueva
+
+    
+# Borrar disciplinas y etiquetas
+@router.delete("/disciplinas/{id}", dependencies=[Depends(verificar_admin)])
+def borrar_disciplina(id: int, db: Session = Depends(get_write_db), admin: Usuario = Depends(usuario_obligatorio)):
+    try:
+        disciplina = db.query(Disciplina).filter(Disciplina.iddisciplina == id).first()
+        db.query(Disciplina).filter(Disciplina.iddisciplina == id).delete()
+        db.commit()
+        logger.info(f"Disciplina borrada {id} - {disciplina.disciplina}, por admin {admin.idusuario} - {admin.email}")
+    except:
+        logger.warning(f"Intento de borrado de disciplina no encontrada con id {id}, por admin {admin.idusuario} - {admin.email}")
+        raise HTTPException(status_code=404, detail="Disciplina no encontrada")
+    
+    return {"ok": True}
+
+@router.delete("/etiquetas/{id}", dependencies=[Depends(verificar_admin)])
+def borrar_etiqueta(id: int, db: Session = Depends(get_write_db), admin: Usuario = Depends(usuario_obligatorio)):
+    try:
+        etiqueta = db.query(Etiqueta).filter(Etiqueta.idetiqueta == id).first()
+        db.query(Etiqueta).filter(Etiqueta.idetiqueta == id).delete()
+        db.commit()
+        logger.info(f"Etiqueta borrada {id} - {etiqueta.etiqueta}, por admin {admin.idusuario} - {admin.email}")
+    except:
+        logger.warning(f"Intento de borrado de etiqueta no encontrada con id {id}, por admin {admin.idusuario} - {admin.email}")
+        raise HTTPException(status_code=404, detail="Etiqueta no encontrada")
+
+    return {"ok": True}
