d6c62bff558bc9cd54d498823f0f3be17a6d104f
ALSREPO <angel.lopez.soto@gmail.com>
2026-05-31 15:53:26 +0200

    feat: añadir seccion Acerca de con enlace al repositorio de GitHub

--- src/routers/frontEnd.py
+++ src/routers/frontEnd.py
@@ -127,3 +127,10 @@ async def pagina_admin_tablas(request: Request, user: Usuario = Depends(obtener_
     if not user or user.activo < 2:
         return RedirectResponse(url="/", status_code=303)
     return templates.TemplateResponse("tecnicas/admin_tablas_bbdd.html", {"request": request, "user": user})
+
+
+########################
+# Página "Acerca de"
+@router.get("/acerca-de", response_class=HTMLResponse)
+def pagina_acerca_de(request: Request, user: Usuario = Depends(obtener_usuario_actual)):
+    return templates.TemplateResponse("acerca_de.html", {"request": request, "user": user})
