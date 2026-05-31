5244b3680215c71d61346b64e276a8f966b3fab6
ALSREPO <angel.lopez.soto@gmail.com>
2026-05-31 20:39:28 +0200

    feat(telegram): sistema de notificación por telegram. Se crea una nueva función, a partir de un nombre y un email, envía un mensaje con botones para aprobar o denegar el registro. Falta implementar la lógica en el bot de telegram para aprobar o denegar. Falta implementar en la web la llamada a la función que envía el mensaje.

--- src/utils/telegram.py
+++ src/utils/telegram.py
@@ -4,6 +4,7 @@ from src.config import TELEGRAM_BOT_TOKEN, TELEGRAM_ADMIN_CHAT_ID
 
 logger = logging.getLogger("AAMM-APP-TELEGRAM-NOTIFICADOR")
 
+# Solo envía mensajes al Administrador del bot de Telegram
 def enviar_alerta_telegram(mensaje: str):
     """Envía un mensaje de notificación al administrador a través de un Bot de Telegram"""
     if not TELEGRAM_BOT_TOKEN or not TELEGRAM_ADMIN_CHAT_ID:
@@ -23,3 +24,44 @@ def enviar_alerta_telegram(mensaje: str):
             logger.error(f"Error de Telegram API: {response.text}")
     except Exception as e:
         logger.error(f"No se pudo enviar la alerta de Telegram: {e}")
+
+
+# A partir de un mail y un nombre, envía una solicitud al admin para aprobar o denegar el registro del usuario
+def enviar_solicitud_registro_telegram(email: str, nombre: str):
+    """Envía una alerta al admin con botones interactivos para activar/denegar al usuario"""
+    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_ADMIN_CHAT_ID:
+        logger.warning("Telegram no configurado.")
+        return
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    
+    # Texto informativo para el Admin
+    texto = (
+        "🆕 *¡Nueva solicitud de registro en la Web!*\n\n"
+        f"👤 *Nombre:* {nombre}\n"
+        f"📧 *Correo:* `{email}`\n\n"
+        "¿Deseas activar esta cuenta para la biblioteca?"
+    )
+
+    # Estructura de los botones interactivos (Inline Keyboard)
+    # Importante: Guardamos la acción y el email en el callback_data
+    inline_keyboard = {
+        "inline_keyboard": [
+            [
+                {"text": "✅ Aprobar", "callback_data": f"aprobar:{email}"},
+                {"text": "❌ Denegar", "callback_data": f"denegar:{email}"}
+            ]
+        ]
+    }
+
+    payload = {
+        "chat_id": TELEGRAM_ADMIN_CHAT_ID,
+        "text": texto,
+        "parse_mode": "Markdown",
+        "reply_markup": inline_keyboard  # Adjuntamos los botones
+    }
+
+    try:
+        requests.post(url, json=payload, timeout=5)
+    except Exception as e:
+        logger.error(f"Error al enviar botones a Telegram: {e}")
