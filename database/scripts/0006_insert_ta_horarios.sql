714e6ad95d42f6bcba9b9d21a1acaecd5c123bdb
ALSREPO <angel.lopez.soto@gmail.com>
2026-06-05 19:02:46 +0200

    feat(bbdd): creación en la BBDD de la tabla de horarios e inserción de datos

---
+++ database/scripts/0006_insert_ta_horarios.sql
@@ -0,0 +1,10 @@
+INSERT IGNORE INTO ta_horarios (actividad, dia_semana, hora_inicio, hora_fin) VALUES
+('Jiu-Jitsu Brasileño', 1, '12:00:00', '13:00:00'),
+('Jiu-Jitsu Brasileño', 1, '13:00:00', '14:00:00'),
+('Jiu-Jitsu Brasileño Junior', 1, '18:30:00', '19:30:00'),
+('MMA - Muay Thai', 1, '19:30:00', '21:00:00'),
+('Jiu-Jitsu Brasileño', 1, '21:00:00', '22:00:00'),
+('MMA - Muay Thai', 2, '12:00:00', '13:00:00'),
+('Luta Livre', 2, '13:00:00', '14:00:00'),
+('Boxeo', 2, '19:30:00', '21:00:00'),
+('Luta Livre', 2, '21:00:00', '22:00:00');
